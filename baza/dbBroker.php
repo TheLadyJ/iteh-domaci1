d0f69b868637e24345173d5851adcce41188d525
TheLadyJ <jovana.vida@gmail.com>
2022-11-12 11:52:52 +0100

    Kreiran broker

---
+++ baza/dbBroker.php
@@ -0,0 +1,38 @@
+<?php
+
+class Broker{
+
+    private $conn = "";
+    private static $broker;
+       
+    private function __construct(){
+        $this->conn = new mysqli("localhost", "root", "", "Kursevi_jezika");
+        $this->conn->set_charset("utf8");
+    }
+
+    public static function getBroker(){
+        if(!isset($broker)){
+            $broker=new Broker();
+        }
+        return $broker;
+    }
+
+    public function sql_query($upit){
+        $odgovor = $this->conn->query($upit);
+        $rezultat = [];
+        if($odgovor){
+            while($red=$odgovor->fetch_assoc()){
+                $rezultat[]=$red;
+            }
+        }
+        else{
+            $rezultat['error']=$this->conn->error;
+        }
+
+        return $rezultat;
+    }
+
+}
+
+
+?>
